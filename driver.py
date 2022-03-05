d6d25d17a2d42cc882a21869de91aee44fdd1a32
AlbinLidback <albin@lidback.com>
2022-03-05 13:43:42 +0100

    try expect

--- driver.py
+++ driver.py
@@ -1,15 +1,42 @@
 #!/usr/bin/env python3
 
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
-barskap_pin = 0
-slinga_pin = 1
+class driver:
+    pins = {"barskap_pin": 0, "slinga_pin": 1}
 
-def toggle(name : str):
-    return 'toggle'
+    def init():
+        try:
+            GPIO.setmode(GPIO.BOARD)
 
-def on(name : str):
-    return 'on'
+            GPIO.setup(barskap_pin, GPIO.OUT)
+            GPIO.setup(slinga_pin, GPIO.OUT)
+        except:
+            return "error"
 
-def off(name : str):
-    return 'off'
+    def toggle(name : str):
+        try:
+            pin = pins[name]
+            if GPIO.input(pin):
+                GPIO.output(pin, False)
+            else:
+                GPIO.output(pin, True)
+            return GPIO.input(pin)
+        except:
+            return "error"
+
+    def on(name : str):
+        try:
+            pin = pins[name]
+            GPIO.output(pin, True)
+            return GPIO.input(pin)
+        except:
+            return "error"
+            
+    def off(name : str):
+        try:
+            pin = pins[name]
+            GPIO.output(pin, False)
+            return GPIO.input(pin)
+        except:
+            return "error"
