d761e22a671fdb29f555251ea5c02d23e2837605
AlbinLidback <albin@lidback.com>
2022-04-20 21:59:10 +0200

    implemented template

--- server.py
+++ server.py
@@ -4,39 +4,50 @@ from bottle import Bottle, response, request, template
 
 app = Bottle()
 
-@app.route('/')
+page_text = {'title': 'PLZ WORK!',
+            }
+
+@app.route('/', method = "GET")
 def index():
     """Home page"""
 
-    info = {'title': 'Welcome Home!',
-            'content': 'Hello World'
-            }
+    #return template('simple.tpl', page_text)
+    return template('simple.tpl', page_text, message="Please enter your name")
 
-    return template('simple.tpl', info)
+@app.route('/', method = "POST")
+def formhandler():
+    """Handle the form submission"""
 
-@app.route('/person/<who:path>')
-def homepage(who):
-    """Generate the home page for a person"""
+    first = request.forms.get('first', type = str)
+    last = request.forms.get('last', type = str)
+    
+    message = "Hello " + first + " " + last + "."
+    
+    return template("simple.tpl",page_text, message=message) 
 
-    return "<p>This is the home page for " + who + ".</p>"
+#@app.route('/person/<who:path>')
+#def homepage(who):
+#    """Generate the home page for a person"""
+#
+#    return "<p>This is the home page for " + who + ".</p>"
 
-def dict_to_html(dd):
-    """Generate an HTML list of the keys and
-    values in the dictionary dd, return a
-    string containing HTML"""
+#def dict_to_html(dd):
+#    """Generate an HTML list of the keys and
+#    values in the dictionary dd, return a
+#    string containing HTML"""
+#
+#    print(dd)
+#
+#    html = "<ul>"
+#    for key in dd.keys():
+#        html += "<li><strong>%s: </strong>%s</li>" % (key, dd[key])
+#    html += "</ul>"
+#    return html
 
-    print(dd)
-
-    html = "<ul>"
-    for key in dd.keys():
-        html += "<li><strong>%s: </strong>%s</li>" % (key, dd[key])
-    html += "</ul>"
-    return html
-
-@app.route('/about')
-def about():
-    response.content_type = 'text/plain'
-    return "Tell me about yourself."
+#@app.route('/about')
+#def about():
+#    response.content_type = 'text/plain'
+#    return "Tell me about yourself."
 
 #@app.route('/about')
 #def about():
--- views/simple.tpl
+++ views/simple.tpl
@@ -1,7 +1,23 @@
 <html>
-    <head><title>{{title}}</title></head>
+    <head>
+        <title>{{title}}</title>
+    </head>
+
     <body>
-         <h1>{{title}}</h1>
-         {{content}}
+        <h1>Beer fridge controller</h1>
+
+        <form method="post" action="/">
+            <fieldset>
+                <legend>SAMPLE FORM</legend>
+                <ul>
+                    <li>First Name: <input name='first'></li>
+                    </li>
+                    <li>Last Name:  <input name='last'></li>
+                    </li>
+                </ul><input type='submit' value='Submit Form'>
+            </fieldset>
+        </form>
+
+        <p>{{message}}</p>
     </body>
 </html>
