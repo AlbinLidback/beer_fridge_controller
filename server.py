008c285c66ca21ecb1fda3643fe95d84d163ef40
Albin Lidbäck <albin@lidback.com>
2022-04-28 21:01:41 +0200

    updated server.py

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/usr/bin/python3
 
 from bottle import Bottle, response, request, template
 import RPi.GPIO as GPIO
+import credentials
 
 app = Bottle()
 
@@ -14,24 +15,25 @@ page_text = {'title': 'Fridge Controller',
 def index():
     """Home page"""
 
-    return template('webpage.html', page_text, message = "")
+    return template('webpage.html', page_text)
+    #return template('webpage.html', page_text, message = "")
 
 @app.route('/', method = "POST")
 def formhandler():
     """Handle the form submission"""
     
-    message = "Received " + request.forms.get('btn')
+    #message = "Received " + request.forms.get('btn')
 
     if request.forms.get('btn') =='ON':
         GPIO.output(2, GPIO.HIGH)
     elif request.forms.get('btn') == 'OFF':
         GPIO.output(2, GPIO.LOW)
     
-    return template('webpage.html', page_text, message = "")
+    return template('webpage.html', page_text)
     
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(2, GPIO.OUT)
 
-    app.run(host='192.168.1.8', port='8080', debug=True, reloader=True)
+    app.run(host = credentials.host, port = credentials.port, debug=True, reloader=True)
